2e69ce1ac905f32799adc0a90807ceaf09baca4a
Antonio Bozzano Schwedhelm <a.bozzanoschwedhel@c013.cip.loc>
2018-03-07 18:20:31 +0100

    some parameters can be set via command line arguments now

--- keras01.py
+++ keras01.py
@@ -7,8 +7,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from keras import backend as K
 import copy
-import sys
-
+import argparse
 
 def print_steps_max(X, model, characters, length=32):
     X=copy.deepcopy(X[0:1])
@@ -32,7 +31,7 @@ def pick_index_from_distribution(dist):
             return i
     return len(dist)-1
 
-def print_steps_probabilistic(X):
+def print_steps_probabilistic(X, model, characters, length=32):
     X=copy.deepcopy(X[0:1])
     steps_num=X.shape[1]
     chars=list(map(lambda x: characters[np.argmax(x)], X[0]))
@@ -80,12 +79,18 @@ get_XYc=lambda offset: get_bookXYchars(filename="prince.txt", training_proportio
 # print (np.prod(X.shape)*(32/8)/1024/1024, "MB")
 # print_steps_max(X[0], characters)
 
-
-activation="tanh" if len(sys.argv)<2 else sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("--activation", type=str, default="linear")
+parser.add_argument("--dropout", type=float, default=.2)
+parser.add_argument("--iterations", type=int, default=40)
+parser.add_argument("--epochs", type=int, default=2)
+parser.add_argument("--lstm_units", type=int, default=80)
+args=parser.parse_args()
+print(args)
 
 model=Sequential()
-model.add(LSTM(80, input_shape=(steps_num, len(characters)), return_sequences=True, dropout=.2, activation=activation)) # maybe use elu? and dropout?
-model.add(LSTM(80, input_shape=(steps_num, len(characters)), return_sequences=True, dropout=.2, activation=activation)) # maybe use elu? and dropout?
+model.add(LSTM(args.lstm_units, input_shape=(steps_num, len(characters)), return_sequences=True, dropout=args.dropout, activation=args.activation)) # maybe use elu? and dropout?
+model.add(LSTM(args.lstm_units, input_shape=(steps_num, len(characters)), return_sequences=True, dropout=args.dropout, activation=args.activation)) # maybe use elu? and dropout?
 model.add(Dense(len(characters), activation="softmax"))
 
 model.compile(loss='categorical_crossentropy', optimizer=SGD(momentum=.99, nesterov=True, decay=0.0001, lr=0.05))
@@ -94,17 +99,34 @@ model.summary()
 losses=list()
 validation_losses=list()
 K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_parallelism_threads=16, inter_op_parallelism_threads=16)))
-for iteration in range(40):
+for iteration in range(args.iterations):
     ((X,Y), (Xval, Yval), _)=get_XYc(iteration)
-    hist=model.fit(X, Y, validation_data=(Xval, Yval), batch_size=20, epochs=2, verbose=1) # more epochs?
+    hist=model.fit(X, Y, validation_data=(Xval, Yval), batch_size=20, epochs=args.epochs, verbose=0) # more epochs?
     print (hist.history)
     losses.extend(hist.history["loss"])
     validation_losses.extend(hist.history["val_loss"])
-    print_steps_max(X, model, characters)
+    print_steps_probabilistic(X, model, characters, length=120)
 
 plt.plot(np.arange(len(losses)), np.array(losses), "g", label="training")
 plt.plot(np.arange(len(validation_losses)), np.array(validation_losses), "b", label="validation")
 x1,x2,y1,y2 = plt.axis()
 plt.axis((x1,x2,0,3))
 plt.legend()
-plt.savefig("plot_%s.png"%activation)
+plt.savefig("plot_%s.png"%str(args))
+
+
+# ["beegfs_server"],["beegfs_storages"],["cpu"],["disk"],["diskio"],["infiniband"],["kernel"],["mem"],["nstat"],["nvidia_gpu"],["nvidia_proc"],["processes"],["swap"],["system"],["uprocstat"]
+# use:
+# cpu: usage_user from cpu-total and the mean, std, and var for all other cpu's (as a measurement for good parallelism)
+# kernel: derivative of context_switches and interrupts
+# mem: cached, used
+# processes: running
+
+
+
+
+
+
+
+
+
